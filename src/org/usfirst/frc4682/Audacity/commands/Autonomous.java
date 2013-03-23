62451ce2a46b23d616d6d9d271bf0915eb5dac2f
Luis Naranjo <luisnaranjo733@gmail.com>
2013-03-23 11:05:14 -0700

    Refined autonomous

--- src/org/usfirst/frc4682/Audacity/commands/Autonomous.java
+++ src/org/usfirst/frc4682/Audacity/commands/Autonomous.java
@@ -5,7 +5,7 @@
 package org.usfirst.frc4682.Audacity.commands;
 
 import org.usfirst.frc4682.Audacity.RobotMap;
-
+import edu.wpi.first.wpilibj.Timer;
 /**
  *
  * @author luis
@@ -15,23 +15,27 @@ public class Autonomous extends CommandBase {
     private int frisbeesShot = 0;
     public Autonomous() {
         requires(shooter);
+        requires(feeder);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        shooter.enable();
+        feeder.enable();
+        shooter.setSpeed(1.0);
+        Timer.delay(1);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         frisbeesShot += 1;
-        shooter.enable();
-        shooter.setSpeed(1.0);
         while (!feeder.atEndLimit()) {
             feeder.setSpeed(-RobotMap.feederSpeed);
         }
         while (!feeder.atStartLimit()) {
             feeder.setSpeed(RobotMap.feederSpeed);
         }
+        Timer.delay(2);
   
     }
 
@@ -42,6 +46,10 @@ public class Autonomous extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+        feeder.stop();
+        shooter.stop();
+        feeder.disable();
+        shooter.disable();
     }
 
     // Called when another command which requires one or more of the same
